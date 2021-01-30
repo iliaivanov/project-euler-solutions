980668a4e932f06017d9e310832c4f373d154201
Ilia Ivanov <ilya.pskov@gmail.com>
2021-01-30 10:06:23 +0200

    Forth problem

--- 4/index.js
+++ 4/index.js
@@ -1,60 +1,56 @@
-const {  } 
+const reverseString = (str) => {
+  if (str === '') {
+    return str
+  }
 
-let initgialDigits = [
-    { key: "firstDigit", value: 999 },
-    { key: "secondDigit", value: 999 },
-    { key: "thirdDigit", value: 999 }
-]
-// TODO: fix - reduce initgialDigits
-let polundrome = firstDigit * secondDigit * thirdDigit
+  return reverseString(str.substr(1)) + str.charAt(0)
+}
 
-const reverseString = str => {
-    if (str === '') {
-        return str
+const checkIfPolundrome = (polundrome) => {
+  polundrome = polundrome.toString()
+
+  return (
+    polundrome.length % 2 == 0 &&
+    polundrome.slice(0, polundrome.length / 2) ==
+      reverseString(polundrome.slice(polundrome.length / 2))
+  )
+}
+
+const getNextPolundrome = (polundrome) => {
+    let nextPolundrome
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
+
+const hasThreeNumberDivisor = (polundrome) => {
+    let found = false
+    let divisor = 999
+    while (!found && divisor > 99) {
+      if (
+        polundrome % divisor == 0 &&
+        (polundrome / divisor).toString().length == 3
+      ) {
+        console.log(`first ${divisor}`)
+        console.log(`second ${polundrome / divisor}`)
+        return true
+      }
+      divisor--
     }
 
-    return reverseString(str.substr(1)) + str.charAt(0)
+    return false
 }
 
-const checkIfPolundrome = polundrome => {
-    polundrome = polundrome.toString()
-    
-    return polundrome.length % 2 == 0 && 
-        polundrome.slice(0, polundrome.length / 2) == reverseString(polundrome.slice(polundrome.length / 2))
+let polundromeFound = false
+let polundrome = 998001 // Max xxx * xxx number
+while (!polundromeFound && polundrome > 10000 ) {
+  polundrome--
+
+  if (checkIfPolundrome(polundrome) && hasThreeNumberDivisor(polundrome)) {
+    console.log(`polundrome ${polundrome} has xxx * xxx combination`)
+    console.log(``)
+    break
+  }
 }
-
-const handleNextDigit = () => {
-    let digits = initgialDigits
-
-    digits.sort((a, b) => {
-        if (a.value < b.value) {
-            return 1
-        }
-
-        if (a.value > b.value) {
-            return -1
-        }
-
-        return 0
-    })
-
-    let nextDigit = digits[0]
-    // TODO: find element with the same key in the initgialDigits and reduce it by 1
-    
-    return 
-}
-
-// Doesn't properly works! Skips possible values. Porbably matrix or smth.
-while (!checkIfPolundrome(polundrome)) {
-    let nextDigit = handleNextDigit()
-    
-    // TODO: fix - reduce initgialDigits
-    polundrome = firstDigit * secondDigit * thirdDigit
-    console.log('firstDigit', firstDigit)
-    console.log('secondDigit', secondDigit)
-    console.log('thirdDigit', thirdDigit)
-    console.log('polundrome', polundrome)
-    
-}
-
-console.log(polundrome)
